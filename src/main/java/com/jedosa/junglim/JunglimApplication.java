65bf71dde00563b5138f0ea5d101e8f8c83a7e33
david <xodnjs2549@naver.com>
2020-11-10 15:27:22 +0900

    first commit
    .gitignore
    readme

---
+++ src/main/java/com/jedosa/junglim/JunglimApplication.java
@@ -0,0 +1,13 @@
+package com.jedosa.junglim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JunglimApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JunglimApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jedosa/junglim/JunglimApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jedosa.junglim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JunglimApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
