fc389147c552bf3d384112411b860c19185729c5
david <xodnjs2549@naver.com>
2020-11-18 17:51:32 +0900

    SCRF 옵션 끄는 것을 추가하라
    해당 옵션이 켜져있으면 H2 웹콘솔로 접근할 수 없다.

--- src/main/java/com/jedosa/junglim/config/SecurityConfig.java
+++ src/main/java/com/jedosa/junglim/config/SecurityConfig.java
@@ -1,7 +1,6 @@
 package com.jedosa.junglim.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -13,8 +12,17 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .mvcMatchers("/", "sign-up", "/lib/**", "/css/**", "/js/**", "/fonts/**").permitAll()
+                // static resources
+                .mvcMatchers("/lib/**", "/css/**", "/js/**", "/fonts/**", "/h2-console/**", "/favicon.ico").permitAll()
+                // views
+                .mvcMatchers("/", "sign-up", "index", "account/**").permitAll()
                 //.mvcMatchers(HttpMethod.GET, "/profile/*").permitAll() // GET 요청일 때만 허용
-                .anyRequest().authenticated();
+                .anyRequest().authenticated()
+//                .and().csrf().ignoringAntMatchers("/h2-console/**")
+                .and().csrf().disable()
+                .headers().frameOptions().disable();
+        //
+//        http.csrf().ignoringAntMatchers("/h2-console/**");
+//        http.headers().frameOptions().disable();
     }
 }
