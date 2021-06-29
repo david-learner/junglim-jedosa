98dd8fc2ddd8e30fa67e26f2ab818ba6e29805d9
david <xodnjs2549@naver.com>
2021-06-29 17:46:47 +0900

    hidden 태그 없애고 data attribute로 데이터를 전달하라

--- src/main/resources/static/js/admin-item.js
+++ src/main/resources/static/js/admin-item.js
@@ -1,69 +1,35 @@
-/**
- * 제본 유형 테이블에 새로운 제본 유형 행을 추가한다
- */
-function addNewBindingType(clickedElement) {
-    // 제본 유형 가격표
+// 인쇄 및 제본 - 제본 유형
+// 새로운 아이템 옵션을 테이블에 추가하기 위해 입력 폼을 생성한다
+function addNewItemOptionForm(clickedElement) {
+    // 추가하려는 아이템 옵션 가격표
     let parent = clickedElement.parentElement.parentElement;
-    // 제본 유형 가격표 마지막 행
-    // 제본 유형 추가 버튼 제외. 테이블에 행이 존재하지 않을 경우 테이블 머리 열이 선택됨
+    // 추가하려는 아이템 옵션 가격표 마지막 행
+    // 옵션 추가 버튼 제외. 테이블에 행이 존재하지 않을 경우 테이블 머리 열이 선택됨
     let lastRowName = parent.lastElementChild.previousElementSibling.getAttribute("name")
-    if (lastRowName === "binding-type-price-table-new-item") {
-        alert("저장되지 않은 제본 유형이 존재합니다.\n저장 후 추가해주세요.");
+    let nameForNotSaved = "new-item-option-template";
+    if (lastRowName === nameForNotSaved) {
+        alert("저장되지 않은 아이템 유형이 존재합니다.\n저장 후 추가해주세요.");
         return;
     }
 
-    // 템플릿을 복사하여 제본 유형 가격 테이블 표에 새로운 행을 추가한다
-    let copiedItem = document.getElementById("binding-type-price-table-new-item").cloneNode(true);
+    // 템플릿을 복사하여 아이템 옵션 가격 테이블 표에 새로운 행을 추가한다
+    let copiedItem = document.getElementById(nameForNotSaved).cloneNode(true);
     copiedItem.style.display = "";
-    // id, name을 삭제하여 새롭게 추가되는 행의 중복을 식별
+    // id는 한 페이지당 하나만 존재해야 하므로 삭제한다
     copiedItem.removeAttribute("id");
-    copiedItem.removeAttribute("name");
     parent.insertBefore(copiedItem, parent.lastElementChild);
 }
 
 /**
- * 제본 유형 테이블에 새로운 제본 유형 행을 삭제한다
+ * 새로운 아이템 옵션 입력 폼을 제거한다
  */
-function removeNewBindingType(clickedElement) {
+function removeNewItemOptionForm(clickedElement) {
     let row = clickedElement.parentElement.parentElement.parentElement.parentElement;
     row.remove();
 }
 
 /**
- * 제본 유형을 저장하고 제본 유형 테이블에 저장된 정보를 추가한다 
- */
-function saveBindingType(clickedElement) {
-    let row = clickedElement.parentElement.parentElement.parentElement.parentElement;
-    let itemId = document.getElementById("item-id").value;
-    let bindingTypeName = row.querySelector("input[name='binding-type-name']").value;
-    let bindingTypePrice = row.querySelector("input[name='binding-type-price']").value;
-
-    let bindingTypeData = {
-        "itemId" : itemId,
-        "name" : bindingTypeName,
-        "price" : bindingTypePrice
-    };
-
-    let requestUrl = "/api/admin/items/" + itemId + "/binding/bindingtypes/save";
-
-    $.ajax({
-        url: requestUrl,
-        dataType: "json",
-        type: 'POST',
-        data: JSON.stringify(bindingTypeData),
-        processData: false,
-        contentType: "application/json"
-    }).done(function (data, textStatus, jqXHR) {
-        alert("저장되었습니다");
-        revealControllerForSaved(row);
-        updateBindingTypeData(row, data);
-    }).fail(function (jqXHR, textStatus, errorThrown) {
-        alert(jqXHR.responseJSON.message);
-    });
-}
-
-/**
- * 새로운 제본유형 저장을 위한 컨트롤러는 제거되고 저장된 제본유형을 위한 컨트롤러가 보여진다
+ * 새로운 아이템 옵션 저장을 위한 컨트롤러는 제거되고 저장된 아이템 옵션을 위한 컨트롤러가 보여진다
  * 입력을 위한 element들은 readonly로 변경된다
  */
 function revealControllerForSaved(row) {
@@ -73,7 +39,7 @@ function revealControllerForSaved(row) {
 }
 
 /**
- * 제본 유형 열의 저장(for업데이트) 컨트롤러를 보여준다
+ * 아이탬 옵션 열의 저장(업데이트) 컨트롤러를 보여준다
  */
 function revealUpdateController(clickedElement) {
     let row = clickedElement.parentElement.parentElement.parentElement.parentElement;
@@ -84,74 +50,143 @@ function revealUpdateController(clickedElement) {
 }
 
 /**
- * 제본 유형 열의 삭제 컨트롤러를 보여준다 
+ * 입력을 취소하고 옵션 아이템 열의 삭제 컨트롤러를 보여준다
  */
 function revealDeleteController(clickedElement) {
     let row = clickedElement.parentElement.parentElement.parentElement.parentElement;
+    restoreOriginalValue(row);
     // controller toggle
     row.querySelector("div[name='updateController']").style.display = "none";
     row.querySelector("div[name='deleteController']").style.display = "";
     toggleInputReadOnly(row);
-}
 
-/**
- * 제본유형 입력 칸을 읽기전용 또는 수정 가능한 상태로 변경한다
- */
-function toggleInputReadOnly(row) {
-    let isNameReadOnly = row.querySelector("input[name='binding-type-name']").getAttribute("readonly");
-    let isPriceReadOnly = row.querySelector("input[name='binding-type-price']").getAttribute("readonly");
-
-    if (isNameReadOnly === null && isPriceReadOnly === null) {
-        row.querySelector("input[name='binding-type-name']").setAttribute("readonly", "")
-        row.querySelector("input[name='binding-type-price']").setAttribute("readonly", "")
-    } else {
-        row.querySelector("input[name='binding-type-name']").removeAttribute("readonly")
-        row.querySelector("input[name='binding-type-price']").removeAttribute("readonly")
+    function restoreOriginalValue(row) {
+        let itemOptionNameElement = row.querySelector("input[name='item-option-name']");
+        let itemOptionPriceElement = row.querySelector("input[name='item-option-price']")
+        let originalItemOptionName = itemOptionNameElement.dataset.itemOptionName;
+        let originalItemOptionPrice = itemOptionPriceElement.dataset.itemOptionPrice;
+        row.querySelector("input[name='item-option-name']").value = originalItemOptionName;
+        row.querySelector("input[name='item-option-price']").value = originalItemOptionPrice;
     }
 }
 
 /**
- * 특정 제본유형 가격표 행의 정보를 업데이트한다
+ * 아이템 옵션 입력 칸을 읽기전용 또는 수정 가능한 상태로 변경한다
  */
-function updateBindingTypeData(row, data) {
-    // data is JsonObject
-    let id = data.id;
-    let name = data.name;
-    let price = data.price;
-    row.querySelector("input[name='binding-type-id']").value = id;
-    row.querySelector("input[name='binding-type-name']").value = name;
-    row.querySelector("input[name='binding-type-price']").value = price;
+function toggleInputReadOnly(row) {
+    let inputOptionName = "item-option-name";
+    let inputOptionPrice = "item-option-price";
+
+    let isNameReadOnly = row.querySelector("input[name='" + inputOptionName + "']").getAttribute("readonly");
+    let isPriceReadOnly = row.querySelector("input[name='" + inputOptionPrice + "']").getAttribute("readonly");
+
+    if (isNameReadOnly === null && isPriceReadOnly === null) {
+        row.querySelector("input[name='" + inputOptionName + "']").setAttribute("readonly", "")
+        row.querySelector("input[name='" + inputOptionPrice + "']").setAttribute("readonly", "")
+    } else {
+        row.querySelector("input[name='" + inputOptionName + "']").removeAttribute("readonly")
+        row.querySelector("input[name='" + inputOptionPrice + "']").removeAttribute("readonly")
+    }
 }
 
 /**
- * 제본유형 정보를 수정한다
+ * 특정 아이템 옵션 가격표 행의 정보를 업데이트한다
  */
-function updateBindingType(clickedElement) {
-    let row = clickedElement.parentElement.parentElement.parentElement.parentElement;
-    let itemId = document.getElementById("item-id").value;
-    let bindingTypeId = row.querySelector("input[name='binding-type-id']").value;
-    let bindingTypeName = row.querySelector("input[name='binding-type-name']").value;
-    let bindingTypePrice = row.querySelector("input[name='binding-type-price']").value;
+function updateItemOptionData(row, data) {
+    // data is JsonObject
+    let id = data.id; // 최초 저장시 id를 화면 내 업데이트 해주기 위해서 필요하다
+    let name = data.name;
+    let price = data.price;
+    let itemOptionNameElement = row.querySelector("input[name='item-option-name']");
+    let itemOptionPriceElement = row.querySelector("input[name='item-option-price']");
+    // update text value
+    itemOptionNameElement.value = name;
+    itemOptionPriceElement.value = price;
+    // update dataset
+    row.dataset.itemOptionId = id;
+    itemOptionNameElement.dataset.itemOptionName = name;
+    itemOptionPriceElement.dataset.itemOptionPrice = price;
+}
 
-    let bindingTypeData = {
+/**
+ * 아이템 옵션을 저장하고 테이블에 옵션 정보가 담긴 행을 추가한다
+ */
+function saveItemOption(clickedElement) {
+    let row = clickedElement.parentElement.parentElement.parentElement.parentElement;
+    let itemOptionCategoryName = row.parentElement.dataset.itemOptionCategoryName;
+    let itemId = document.getElementById("item-id").value;
+    let itemOptionName = row.querySelector("input[name='item-option-name']").value;
+    let itemOptionPrice = row.querySelector("input[name='item-option-price']").value;
+
+    // validation
+    let allowOnlyNumbersRegex = new RegExp(/^[0-9]*$/);
+    if (itemOptionPrice === "" || itemOptionPrice === undefined || !allowOnlyNumbersRegex.test(itemOptionPrice)) {
+        alert("상품 옵션 가격은 숫자만 입력할 수 있습니다.");
+        return;
+    }
+
+    let itemOptionData = {
         "itemId" : itemId,
-        "id" : bindingTypeId,
-        "name" : bindingTypeName,
-        "price" : bindingTypePrice
+        "name" : itemOptionName,
+        "price" : itemOptionPrice
     };
 
-    let requestUrl = "/api/admin/items/" + itemId + "/binding/bindingtypes/" + bindingTypeId;
+    let requestUrl = "/api/admin/items/" + itemId + "/binding/" + itemOptionCategoryName;
+
+    $.ajax({
+        url: requestUrl,
+        dataType: "json",
+        type: 'POST',
+        data: JSON.stringify(itemOptionData),
+        processData: false,
+        contentType: "application/json"
+    }).done(function (data, textStatus, jqXHR) {
+        alert("저장되었습니다");
+        revealControllerForSaved(row);
+        updateItemOptionData(row, data);
+        removeRowNameForRecognizingSavedItemOption(row);
+    }).fail(function (jqXHR, textStatus, errorThrown) {
+        alert(jqXHR.responseJSON.message);
+    });
+}
+
+/**
+ * 저장된 아이템 옵션 행(row)임을 알기 위해서 new item option에만 붙는 이름을 제거한다
+ */
+function removeRowNameForRecognizingSavedItemOption(element) {
+    element.removeAttribute("name");
+}
+
+/**
+ * 특정 아이템 옵션 정보를 수정한다
+ */
+function updateItemOption(clickedElement) {
+    let row = clickedElement.parentElement.parentElement.parentElement.parentElement;
+    let itemId = document.getElementById("item-id").value;
+    let itemOptionCategoryName = row.parentElement.dataset.itemOptionCategoryName;
+    let itemOptionId = row.dataset.itemOptionId;
+    let itemOptionName = row.querySelector("input[name='item-option-name']").value;
+    let itemOptionPrice = row.querySelector("input[name='item-option-price']").value;
+
+    let itemOptionData = {
+        "itemId" : itemId,
+        "id" : itemOptionId,
+        "name" : itemOptionName,
+        "price" : itemOptionPrice
+    };
+
+    let requestUrl = "/api/admin/items/" + itemId + "/binding/" + itemOptionCategoryName + "/" + itemOptionId;
 
     $.ajax({
         url: requestUrl,
         dataType: "json",
         method: 'PUT',
-        data: JSON.stringify(bindingTypeData),
+        data: JSON.stringify(itemOptionData),
         processData: false,
         contentType: "application/json"
     }).done(function (data, textStatus, jqXHR) {
         alert("수정되었습니다");
-        updateBindingTypeData(row, data);
+        updateItemOptionData(row, data);
         revealDeleteController(clickedElement);
     }).fail(function (jqXHR, textStatus, errorThrown) {
         alert(jqXHR.responseJSON.message);
@@ -159,20 +194,21 @@ function updateBindingType(clickedElement) {
 }
 
 /**
- * 제본유형 정보를 삭제한다
+ * 특정 아이템 옵션을 삭제한다
  */
-function deleteBindingType(clickedElement) {
+function deleteItemOption(clickedElement) {
     let row = clickedElement.parentElement.parentElement.parentElement.parentElement;
     let itemId = document.getElementById("item-id").value;
-    let bindingTypeId = row.querySelector("input[name='binding-type-id']").value;
-    let bindingTypeName = row.querySelector("input[name='binding-type-name']").value;
-    let inputName = prompt("정말 삭제하시겠습니까?\n삭제하려는 제본유형 이름을 적어주세요", "");
-    if (bindingTypeName !== inputName) {
-        alert("삭제하려는 제본유형 이름이 일치하지 않습니다.");
+    let itemOptionCategoryName = row.parentElement.dataset.itemOptionCategoryName;
+    let itemOptionId = row.dataset.itemOptionId;
+    let itemOptionName = row.querySelector("input[name='item-option-name']").value;
+    let inputName = prompt("정말 삭제하시겠습니까?\n삭제하려는 상품 옵션의 이름을 적어주세요", "");
+    if (itemOptionName !== inputName) {
+        alert("삭제하려는 상품 옵션의 이름과 일치하지 않습니다.");
         return;
     }
 
-    let requestUrl = "/api/admin/items/" + itemId + "/binding/bindingtypes/" + bindingTypeId;
+    let requestUrl = "/api/admin/items/" + itemId + "/binding/" + itemOptionCategoryName + "/" + itemOptionId;
 
     $.ajax({
         url: requestUrl,
