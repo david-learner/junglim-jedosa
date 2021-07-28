85ce491640d282ec36931ae8850f0b209eb441bd
david <xodnjs2549@naver.com>
2021-07-28 09:58:25 +0900

    에러 메시지 한글을 영어로 변경

--- src/main/java/com/jedosa/junglim/exception/NoLoginException.java
+++ src/main/java/com/jedosa/junglim/exception/NoLoginException.java
@@ -3,6 +3,6 @@ package com.jedosa.junglim.exception;
 public class NoLoginException extends RuntimeException {
 
     public NoLoginException() {
-        super("로그인이 필요한 서비스입니다");
+        super("NEED LOGIN");
     }
 }
