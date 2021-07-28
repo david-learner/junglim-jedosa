38ac9d8606a13861430f07968459c9ec64dcbb41
david <xodnjs2549@naver.com>
2021-07-28 14:58:44 +0900

    예외발생시 에러처리 방식 변경
    BasicErrorController에 맡기는 대신, view를 지정해서 오류 페이지 제공

--- src/main/java/com/jedosa/junglim/exception/NoLoginException.java
+++ src/main/java/com/jedosa/junglim/exception/NoLoginException.java
@@ -3,6 +3,6 @@ package com.jedosa.junglim.exception;
 public class NoLoginException extends RuntimeException {
 
     public NoLoginException() {
-        super("NEED LOGIN");
+        super("로그인이 필요한 서비스입니다");
     }
 }
--- src/main/java/com/jedosa/junglim/infrastructure/GlobalExceptionController.java
+++ src/main/java/com/jedosa/junglim/infrastructure/GlobalExceptionController.java
@@ -1,11 +1,14 @@
 package com.jedosa.junglim.infrastructure;
 
 import com.jedosa.junglim.exception.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
@@ -13,53 +16,94 @@ import java.io.IOException;
 @ControllerAdvice
 public class GlobalExceptionController {
 
+    private static final Logger log =  LoggerFactory.getLogger(GlobalExceptionController.class);
+
     @ExceptionHandler(IllegalArgumentException.class)
-    public void illegalArgumentException(HttpServletResponse response, IllegalArgumentException ex, Model model) throws IOException {
-        response.sendError(HttpStatus.BAD_REQUEST.value(), ex.getMessage());
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public String illegalArgumentException(final Throwable throwable, final Model model) throws IOException {
+        log.error("예외발생", throwable);
+        model.addAttribute("exception", throwable.getClass());
+        model.addAttribute("message", throwable.getMessage());
+        return "error/4xx";
     }
 
     @ExceptionHandler(NoLoginException.class)
-    public void noLoginException(HttpServletResponse response, NoLoginException ex) throws IOException {
-        response.sendError(HttpStatus.BAD_REQUEST.value(), ex.getMessage());
+    @ResponseStatus(HttpStatus.UNAUTHORIZED)
+    public String noLoginException(final Throwable throwable, final Model model) {
+        log.error("예외발생", throwable);
+        model.addAttribute("exception", throwable.getClass());
+        model.addAttribute("message", throwable.getMessage());
+        return "error/4xx";
     }
 
     @ExceptionHandler(NotAdminException.class)
-    public void notAdminException(HttpServletResponse response, NotAdminException ex) throws IOException {
-        response.sendError(HttpStatus.BAD_REQUEST.value(), ex.getMessage());
+    @ResponseStatus(HttpStatus.UNAUTHORIZED)
+    public String notAdminException(final Throwable throwable, final Model model) throws IOException {
+        log.error("예외발생", throwable);
+        model.addAttribute("exception", throwable.getClass());
+        model.addAttribute("message", throwable.getMessage());
+        return "error/4xx";
     }
 
     @ExceptionHandler(NoOnePageMenuException.class)
-    public void noOnePageMenuException(HttpServletResponse response, NoOnePageMenuException ex) throws IOException {
-        response.sendError(HttpStatus.INTERNAL_SERVER_ERROR.value(), ex.getMessage());
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public String noOnePageMenuException(final Throwable throwable, final Model model) throws IOException {
+        log.error("예외발생", throwable);
+        model.addAttribute("exception", throwable.getClass());
+        model.addAttribute("message", throwable.getMessage());
+        return "error/5xx";
     }
 
     @ExceptionHandler(NoArticleException.class)
-    public void noArticleException(HttpServletResponse response, NoArticleException ex) throws IOException {
-        response.sendError(HttpStatus.BAD_REQUEST.value(), ex.getMessage());
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public String noArticleException(final Throwable throwable, final Model model) throws IOException {
+        log.error("예외발생", throwable);
+        model.addAttribute("exception", throwable.getClass());
+        model.addAttribute("message", throwable.getMessage());
+        return "error/4xx";
     }
 
     @ExceptionHandler(NoAccountException.class)
-    public void noAccountException(HttpServletResponse response, NoAccountException ex) throws IOException {
-        response.sendError(HttpStatus.BAD_REQUEST.value(), ex.getMessage());
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public String noAccountException(final Throwable throwable, final Model model) throws IOException {
+        log.error("예외발생", throwable);
+        model.addAttribute("exception", throwable.getClass());
+        model.addAttribute("message", throwable.getMessage());
+        return "error/4xx";
     }
 
     @ExceptionHandler(NotOwnException.class)
-    public void notOwnException(HttpServletResponse response, NotOwnException ex) throws IOException {
-        response.sendError(HttpStatus.BAD_REQUEST.value(), ex.getMessage());
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public String notOwnException(final Throwable throwable, final Model model) throws IOException {
+        log.error("예외발생", throwable);
+        model.addAttribute("exception", throwable.getClass());
+        model.addAttribute("message", throwable.getMessage());
+        return "error/4xx";
     }
 
     @ExceptionHandler(NoCommentException.class)
-    public void noCommentException(HttpServletResponse response, NoCommentException ex) throws IOException {
-        response.sendError(HttpStatus.BAD_REQUEST.value(), ex.getMessage());
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public String noCommentException(final Throwable throwable, final Model model) throws IOException {
+        log.error("예외발생", throwable);
+        model.addAttribute("exception", throwable.getClass());
+        model.addAttribute("message", throwable.getMessage());
+        return "error/4xx";
     }
 
     @ExceptionHandler(AlreadyOrderedException.class)
-    public void alreayOrderedException(HttpServletResponse response, AlreadyOrderedException ex) throws IOException {
-        response.sendError(HttpStatus.BAD_REQUEST.value(), ex.getMessage());
-    }
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public String alreayOrderedException(final Throwable throwable, final Model model) throws IOException {
+        log.error("예외발생", throwable);
+        model.addAttribute("exception", throwable.getClass());
+        model.addAttribute("message", throwable.getMessage());
+        return "error/4xx";    }
 
     @ExceptionHandler(NoFlyleafContentPriceException.class)
-    public void noFlyleafContentPriceException(HttpServletResponse response, NoFlyleafContentPriceException ex) throws IOException {
-        response.sendError(HttpStatus.BAD_REQUEST.value(), ex.getMessage());
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public String noFlyleafContentPriceException(final Throwable throwable, final Model model) throws IOException {
+        log.error("예외발생", throwable);
+        model.addAttribute("exception", throwable.getClass());
+        model.addAttribute("message", throwable.getMessage());
+        return "error/4xx";
     }
 }
